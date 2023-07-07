21ac778233804f5e3aaa75ba5da4d0e4afb97a65
Srinuthimmisetty <138871587+Srinuthimmisetty@users.noreply.github.com>
2023-07-07 15:48:04 +0530

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,135 @@
+function clock()
+{
+    var date=new Date();
+    var hh=date.getHours();
+    var mm=date.getMinutes();
+    var ss=date.getSeconds();
+    var day=date.getDay();
+    var dd=date.getDate();
+    var mo=date.getDate();
+    var yy=date.getMonth();
+    var am_pm="AM";
+    if(hh>=12)
+    {
+        am_pm="PM";
+        if(hh>12)
+        {
+            hh=hh-12;
+        }
+    }
+    if(hh==0)
+    {
+        hh=12;
+    }
+    if(mo==0)
+    {
+        mo="Jan"
+    }
+    else if(mo==1){
+        mo="Feb"
+    }
+    else if(mo==2){
+        mo="Mar"
+    }
+    else if(mo==3){
+        mo="Apr"
+    }
+    else if(mo==4){
+        mo="May"
+    }
+    else if(mo==5){
+        mo="Jun"
+    }
+    else if(mo==6){
+        mo="Jul"
+    }
+    else if(mo==7){
+        mo="Aug"
+    }
+    else if(mo==8){
+        mo="Sep"
+    }
+    else if(mo==9){
+        mo="Oct"
+    }
+    else if(mo==10){
+        mo="Nov"
+    }
+    else{
+        mo="Dec"
+    }
+    console.log(mo);
+    switch(day)
+    {
+        case 0: day="Sunday"
+            document.body.style.backgroundImage="url(./chitralahari.jpg)";
+            break;
+        case 1: day="Monday"
+            document.body.style.backgroundImage="url(./feed.jpg)";
+            break;
+        case 2:day="Tuesday"
+            document.body.style.backgroundImage="url(./home.jpg)";
+            break;
+        case 3:day="Wednesday"
+            document.body.style.backgroundImage="url(./KGF.jpg)";
+            break;
+        case 4:day="Thusday"
+            document.body.style.backgroundImage="url(./info.jpg)";
+            break;
+        case 5:day="Friday"
+            document.body.style.backgroundImage="url(./pushpa.jpg)";
+            break;
+        case 6:day="saterday"
+            document.body.style.backgroundImage="url(./Soorarai Pottru.jpg)";
+    }
+    console.log(day);
+    document.getElementById("time").innerHTML=`${hh}:${mm}`
+    document.getElementById("date").innerHTML=`${dd}/${mo}/${yy}`
+    document.getElementById("day").innerHTML=day;
+    document.getElementById("sec").innerHTML=ss
+    setTimeout(clock,1000)
+}
+clock()
+var audio=new Audio();
+audio.src="./alarm-clock-short-6402.mp3"
+function alarmFunction(){
+    var date=new Date();
+    var hh=date.getHours();
+    var mm=date.getMinutes();
+    var am_pm="AM";
+    if(hh>=12)
+    {
+        am_pm="PM";
+        if(hh>12)
+        {
+            hh=hh-12;
+        }
+    }
+    if(hh==0)
+    {
+        hh=12;
+    }
+    var userGivenhh=document.getElementById("userHours").value
+    var userGivenmm=document.getElementById("userMin").value
+    var userGivenam_pm=document.getElementById("userAm_Pm").value
+    console.log(hh,mm,am_pm)
+    console.log(userGivenhh,userGivenmm,userGivenam_pm)
+    if(hh==userGivenhh && mm==userGivenmm && am_pm==userGivenam_pm)
+    {
+        audio.play();
+        audio.loop=Infinity
+    }
+    else
+    {
+        audio.pause();
+    }
+    setTimeout(alarmFunction,1000)
+}
+function closeWindow()
+{
+    document.getElementById("alarmContainer").style.display="none"
+}
+function openWindow()
+{
+    document.getElementById("alarmContainer").style.display="flex"
+}
